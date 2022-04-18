9db196754020960cfb3d9149c3c23baf5dea8e76
denzildiniz <denzildiniz73@gmail.com>
2022-04-18 13:38:00 +0530

    basic setupp

--- app.js
+++ app.js
@@ -1,12 +1,37 @@
+require('dotenv').config();
+require('express-async-errors')
 
+// express 
 const express = require('express');
 const app = express();
 
+// packages 
+const { StatusCodes } = require('http-status-codes');
+
+// dataBase 
+const connDb = require('./db/connect')
+
+// middleware
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
 app.use(express.json())
 
+app.get("/", (req, res) => {
+  res.status(StatusCodes.OK).send("get route");
+});
 
-const port = process.env.Port || 4000;
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
-app.listen(port,()=>
-console.log(`app listening on port ${port}`)
-)
+const port = process.env.PORT || 4000;
+const start = async () => {
+  try {
+    await connDb(process.env.MONGO_URL);
+    app.listen(port, () => console.log(`app listening on port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
