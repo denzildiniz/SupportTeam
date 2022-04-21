5c746774e476c2760178e1233fa257321f4d1a99
denzildiniz <denzildiniz73@gmail.com>
2022-04-21 15:11:12 +0530

    jwt assigned on login

--- controllers/user.js
+++ controllers/user.js
@@ -1,13 +1,45 @@
-
+const User = require('../models/user')
+const { StatusCodes } = require("http-status-codes");
+const CustomError = require('../errors')
 
 const registerUser = async (req, res) => {
-  console.log(req.body);
-  res.status(201).send('register')
+  // console.log(req.body)
+  const {fname, lname ,password , branch, email} = req.body;
+
+  const emailExist = await User.findOne({email});
+  if(emailExist) {
+    throw new CustomError.BadRequestError('Email already in Use');
+  }
+
+   // first account to register as superAdmin
+   const isFirstAccount = (await User.countDocuments({})) === 0;
+   const role = isFirstAccount ? 'superadmin' : 'user';
+
+   const user = await User.create({fname, lname ,password , branch, email ,role});
+   res.status(StatusCodes.CREATED).json({msg:"User created Successfully"})
 };
 
 const loginUser = async (req, res) => {
     console.log(req.body);
-  res.status(201).json('login')
+    const {email , password } = req.body;
+
+    if(!email || !password){
+      throw new CustomError.BadRequestError('Please provide Email and Password')
+    }
+
+    const user = await User.findOne({email});
+    if(!user){
+      throw new CustomError.UnauthenticatedError('Invalid credentionals');
+    }
+
+    const correctPassword = await user.comparePasword(password);
+    if(!correctPassword){
+      throw new CustomError.UnauthenticatedError('Invalid Credentionals')
+    }
+
+    const token = await user.createJWT();
+    res.status(StatusCodes.ACCEPTED).json({token})
+
 };
 
 const logoutUser = async (req, res) => {
--- models/user.js
+++ models/user.js
@@ -1,52 +1,75 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
+const jwt = require('jsonwebtoken')
 
-const userSchema = mongoose.Schema({
-  fname: {
-    type: String,
-    required: [true, "Please provide name"],
-    maxlength: 50,
-    minlength: 3,
-  },
-  lname: {
-    type: String,
-    required: [true, "Please provide name"],
-    maxlength: 50,
-    minlength: 3,
-  },
-  password: {
-    type: String,
-    required: [true, "please provide a password"],
-    maxlength: 50,
-    minlength: 3,
-  },
-  branch: {
-    type: String,
-    enum: ["goa", "dhaka", "sylhet"],
-    default: "goa",
-  },
-  email: {
-    type: String,
-    unique: true,
-    required: [true, "Please provide email"],
-    validate: {
-      validator: validator.isEmail,
-      message: "Please provide valid email",
+const userSchema = new mongoose.Schema(
+  {
+    fname: {
+      type: String,
+      required: [true, "Please provide name"],
+      maxlength: 50,
+      minlength: 3,
+    },
+    lname: {
+      type: String,
+      required: [true, "Please provide name"],
+      maxlength: 50,
+      minlength: 3,
+    },
+    password: {
+      type: String,
+      required: [true, "please provide a password"],
+      maxlength: 50,
+      minlength: 3,
+    },
+    branch: {
+      type: String,
+      required: [true, "please provide a branch name"],
+      enum: ["goa", "dhaka", "sylhet"],
+      default: "goa",
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: [true, "Please provide email"],
+      validate: {
+        validator: validator.isEmail,
+        message: "Please provide valid email",
+      },
+    },
+    status: {
+      type: String,
+      enum: ["active", "inactive"],
+      default: "active",
+    },
+    role: {
+      type: String,
+      enum: ["superadmin", "admin", "user"],
+      default: "user",
     },
   },
-  status: {
-    type: String,
-    enum: ["active", "inactive"],
-    default: "active",
-  },
-  role: {
-    type: String,
-    enum: ["superadmin", "admin", "user"],
-    default: "user",
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", async function () {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
+
+userSchema.methods.comparePasword = async function(userPassword){
+  const isMatch = await bcrypt.compare(userPassword,this.password)
+  return isMatch;
+}
+
+userSchema.methods.createJWT = function () {
+  return jwt.sign(
+    { userId: this._id, email: this.email, role:this.role, branch:this.branch},
+    process.env.JWT_SECRET,
+    {
+      expiresIn: process.env.JWT_LIFETIME,
+    }
+  )
+}
 
 module.exports = mongoose.model("User", userSchema);
