dc95687df0f407d04d57e52f14b0130afb6c79f9
denzildiniz <denzildiniz73@gmail.com>
2022-05-06 15:11:02 +0530

    preparing to deploy on heroku

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 require("dotenv").config();
 require("express-async-errors");
 
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+
 // express
 const express = require("express");
 const app = express();
@@ -19,13 +24,25 @@ const userRoute = require("./routes/userRote");
 // middleware
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
-const req = require("express/lib/request");
+// const req = require("express/lib/request");
 
+app.set("trust proxy", 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  })
+);
 app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(xss());
 app.use(morgan("tiny"));
 
 app.get("/", (req, res) => {
-  res.status(StatusCodes.OK).send("get route");
+  res.status(StatusCodes.OK).send("first route");
 });
 
 app.use("/api/st/auth", authRoute);
