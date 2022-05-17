96818c8b503b249549866febc4a4c75570735ffb
denzildiniz <denzildiniz73@gmail.com>
2022-05-17 14:07:54 +0530

    assigned devices controller

--- controllers/assignedProductController.js
+++ controllers/assignedProductController.js
@@ -1,9 +1,70 @@
 const AssignedProduct = require("../models/assignedProduct");
+const User = require("../models/user");
+const Product = require("../models/product");
 const { StatusCodes } = require("http-status-codes");
 const CustomError = require("../errors");
 
 const createAssignedProduct = async (req, res) => {
-  res.send("create assigned prodduct");
+  const { branch, user: userId, assignedDevices, assignedBy } = req.body;
+  if (!branch || !userId) {
+    throw new CustomError.BadRequestError("Please provide branch and userId");
+  }
+
+  if (!assignedDevices || assignedDevices.length < 1) {
+    throw new CustomError.BadRequestError(
+      "Please provide the product details to assign"
+    );
+  }
+
+  const verifyuser = await User.findOne({ _id: userId });
+  if (!verifyuser) {
+    throw new CustomError.NotFoundError(`No user found with id ${userId}`);
+  }
+  if (verifyuser.branch !== branch) {
+    throw new CustomError.BadRequestError("User needs to be from same branch");
+  }
+  if (verifyuser.role === "superadmin" && req.user.role === "admin") {
+    throw new CustomError.UnauthorizedError(
+      "Not authorize to proceed with this task"
+    );
+  }
+  if (verifyuser.role === "admin" && req.user.role === "admin") {
+    throw new CustomError.UnauthorizedError(
+      "Not authorize to proceed with this task"
+    );
+  }
+
+  let devicesToAssign = [];
+  for (const singledevice of assignedDevices) {
+    const dbProduct = await Product.findOne({ _id: singledevice.product });
+    if (!dbProduct) {
+      throw new CustomError.NotFoundError(
+        `No product found with id ${device.product}`
+      );
+    }
+    if (dbProduct.branch !== branch) {
+      throw new CustomError.BadRequestError(
+        "Product needs to be from same branch"
+      );
+    }
+    const { device, _id } = dbProduct;
+    const singleProduct = {
+      device,
+      product: _id.toString(),
+    };
+    devicesToAssign = [...devicesToAssign, singleProduct];
+  }
+
+  const assignedUserDevice = {
+    branch,
+    user: userId,
+    assignedDevices: devicesToAssign,
+    assignedBy: req.user.userId,
+  };
+
+  console.log(devicesToAssign);
+  console.log(req.body);
+  res.send(req.body);
 };
 
 const getAllAssignedProduct = async (req, res) => {
